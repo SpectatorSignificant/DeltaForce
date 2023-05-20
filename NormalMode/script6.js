60d7c66010f94cabd388b005d5c2ec89262a704a
SpectatorSignificant <97103959+SpectatorSignificant@users.noreply.github.com>
2023-05-20 23:46:14 +0530

    Update script6.js

--- NormalMode/script6.js
+++ NormalMode/script6.js
@@ -1,12 +1,13 @@
+//defining HTML elements
 const playBoard = document.querySelector(".play-board");
 const scoreElement = document.querySelector(".score");
-//const highScoreElement = document.querySelector(".high-score");
 const controls = document.querySelectorAll(".controls i");
 const timer = document.querySelector(".timer");
 const sequenceBlock = document.querySelector(".sequence");
 const foodSound = new Audio("food.mp3");
 const gameOverSound = new Audio("gameover.mp3");
 
+//defining game variables
 let startTime, endTime, duration;
 let timeLeft = 60;
 let gridsize = 20;
@@ -22,37 +23,21 @@ let setIntervalId;
 let score = 0;
 let lives=3;
 
+
 const leaderTable = document.querySelector(".leaderboard");
-//localStorage.removeItem("leaderboard");
-//let leaderList = null;
-//localStorage.removeItem("leaderboard");
 let leaderList = JSON.parse(localStorage.getItem("leaderboard"));
-//console.log(leaderList,typeof(leaderList));
 if (leaderList === null || leaderList == []){
     leaderList = [];
     for (let i = 0; i < 10; i++){
         leaderList.push([0, "Guest"]);
     }
-    //console.log(leaderList, typeof(leaderList));
     localStorage.setItem("leaderboard", JSON.stringify(leaderList));
 }
-//console.log(leaderList, typeof(leaderList));
-//let list = [[1,2],[3,4]]
-//console.log(list, typeof(list));
-/*leaderList.forEach((element) => {
-    console.log(element[0], element[1]);
-});*/
 
-
-//localStorage.setItem("high-score", 3);
-//Changing gridsize
 let username = prompt("Please enter your username", "Guest");
 playBoard.style['grid-template'] = `repeat(${gridsize}, 1fr) / repeat(${gridsize}, 1fr)`;
-//playBoard.style.setAttribute("style",`grid-template: repeat(${gridsize}, 1fr) / repeat(${gridsize}, 1fr)`);
 
-//let highScore = localStorage.getItem("high-score") || 0;
-//highScoreElement.innerText = `High Score: ${highScore}`;
-
+//defining a function to find to if an element already exists in an array
 const ifAlreadyPresent = (a, arr) => {
     for (let i = 0; i < arr.length; i++){
         if (a[0] == arr[i][0] && a[1] == arr[i][1]){
@@ -61,28 +46,13 @@ const ifAlreadyPresent = (a, arr) => {
     }
     return false;
 }
+
+//generates new coordinates for 5 food items
 const updateFoodPosition = () => {
-    // Passing a random 1 - gridsize value as food position
-    //foodX = 10;
-    //foodY = 10;
-    //food[0] = [10, 10];
-    //food[1] = [15, 15];
     while (food.length < 5){
         foodX = Math.floor(Math.random() * gridsize) + 1;
         foodY = Math.floor(Math.random() * gridsize) + 1;
-        /*food2.forEach((element, index) => {
-            if (foodX != element[0] || foodY != element[1]){
-                console.log([foodX, foodY]);
-                food.push([foodX, foodY]);
-                food2.push([foodX, foodY]);
-                alert(food2);
-            }
-        });*/
-        /*if (!(food2.includes([foodX, foodY]))){
-            console.log([foodX, foodY], food2.includes([foodX, foodY]));
-            food.push([foodX, foodY]);
-            food2.push([foodX, foodY]);
-        }*/
+     
         if (ifAlreadyPresent([foodX, foodY], food2) || ifAlreadyPresent([foodX, foodY], snakeBody) || (foodX == snakeX && foodY == snakeY)){
             continue;
         }
@@ -92,13 +62,11 @@ const updateFoodPosition = () => {
         }
     }
     food.forEach((element, index) => {
-        //alert(element, element.push(index));
         element.push(index);
         food.splice(index,1,element);
     
     });
-    //alert(food2);
-    let html = "";
+    let html = ""; //adding the food tiles to the bottom of the screen
     food.forEach((element, index) => {
         html+= `<div class="food${element[2]+1} tile" style="grid-area: ${element[1]} / ${element[0]}"></div>`;
     });
@@ -106,20 +74,21 @@ const updateFoodPosition = () => {
 
 }
 
-const handleGameOver = () => {
+//g
+const whenGameOver = () => {
     // Clearing the timer and reloading the page on game over
-    leaderList.push([score, username]);
+    leaderList.push([score, username]); //updates the leaderboard
     leaderList.sort((a,b) => {return a[0]-b[0]});
     leaderList.reverse();
     leaderList.pop();
     localStorage.setItem("leaderboard", JSON.stringify(leaderList));
     if (timeLeft > 0){
-        gameOverSound.play();
+        gameOverSound.play(); //sound of death
     }
-    //console.log(leaderList[0]);
-    clearInterval(setIntervalId);
+
+    clearInterval(setIntervalId); //ends the loop
     alert("Game Over! Press OK to replay...");
-    location.reload();
+    location.reload(); //reloads the page
 }
 
 const changeDirection = (e) => {
@@ -143,158 +112,57 @@ const changeDirection = (e) => {
         velocityX = 1; 
         velocityY = 0;
     }
-    //console.log(leaderList[0]);
     if (timeLeft == 60 && (velocityX != 0 || velocityY != 0)){
         startTime = new Date();
-        //endTime = new Date();
-        //duration = endTime - startTime
-        timeLeft = 59.99;
-        //timer.innerText = `Time : ${timeLeft}`;
+        timeLeft = 59.99; //timer begins only after first keypress
     }
-    //console.log(leaderList[0]);
 }
 
 // Calling changeDirection on each key click and passing key dataset value as an object
 controls.forEach(button => button.addEventListener("click", () => changeDirection({ key: button.dataset.key })));
 
-const pauseButton = document.querySelector(".pause");
-const saveButton = document.querySelector(".save");
-
-const initGame = () => {
+const gameEngine = () => {
     if (timeLeft != 60){
-        //console.log(timeLeft);
         endTime = new Date();
         duration = (endTime - startTime)/1000;
         startTime = new Date();
         timeLeft -= duration;
-        timer.innerText = `Time: ${Math.round(timeLeft)}`;
+        timer.innerText = `Time: ${Math.round(timeLeft)}`;//updates timer display
         if (duration/1000 >= 60){
-            //console.log(duration);
             gameOver = true;
         }
     }
-    //console.log(duration);
 
     if(gameOver){
-        return handleGameOver();
+        return whenGameOver();
     }
     else if (play==false){
         return;
     }
     let html = "";
-    //let html = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
 
     // Checking if the snake hit the food
-    
-    //let foodNumber = 1;
     for (let i = 0; i < food.length; i++){
-        html+= `<div class="food${food[i][2]+1}" style="grid-area: ${food[i][1]} / ${food[i][0]}"></div>`;
+        html+= `<div class="food${food[i][2]+1}" style="grid-area: ${food[i][1]} / ${food[i][0]}"></div>`;//adds food elements to the board
         if (snakeX == food[i][0] && snakeY == food[i][1]){
             foodSound.play();
-            //console.log((i+ 1), foodNumber);
-            if (i != 0){
+            if (i != 0){ //checks if color was eaten in the right order
                 gameOver = true;
-                //foodNumber = 1;
-                //console.log((i + 1), foodNumber);
                 break;
             }
-            //updateFoodPosition();
-            /*if (i + 1 != foodNumber){
-                gameOver = true;
-                foodNumber = 1;
-                console.log((i + 1), foodNumber);
-                break;
-            }*/
             else{
-                //foodNumber ++;
-                //console.log((i + 1), foodNumber);
-                //alert(index + 1);
-                /*if (foodNumber == 6){
-                    console.log(i + 1);
-                    score = score + 3; // 2 extra points for completing a sequence
-                }*/
-                score++; //increment score by 1 for eating a block
-                /*if (score > highScore){
-                    highScore = score;
-                }*/
-                //snakeBody.push([[element[0], element[1]]]); // Pushing food position to snake body array
-                food.splice(i,1);
+                food.splice(i,1); //
                 if (food.length == 0){
                     score += 2;
-                    //console.log(score, timeLeft);
                     timeLeft += 5;
-                    //console.log(score, timeLeft);
-                    updateFoodPosition();            
+                    updateFoodPosition();//generates new food    
                 }
-                //console.log("hello")
                 break;
         }
         }
-        scoreElement.innerText = `Score: ${score}`;
+        scoreElement.innerText = `Score: ${score}`;//updates score display
     }
-    /*if(snakeX === foodX && snakeY === foodY) {
-        foodSound.play();
-        updateFoodPosition();
-        snakeBody.push([foodX,foodY]); // Pushing food position to snake body array
-        score++; // increment score by 1
-        if (score > highScore){
-            highScore = score;
-
-        }*/
     
-        //localStorage.setItem("high-score", highScore);
-        //highScoreElement.innerText = `High Score: ${highScore}`;
-    
-    /*food.forEach((element, index) => {
-        //alert(food);
-        //alert(element[3]);
-        html+= `<div class="food${element[2]+1}" style="grid-area: ${element[1]} / ${element[0]}"></div>`;
-        if (snakeX == element[0] && snakeY == element[1]){
-            foodSound.play();
-            //updateFoodPosition();
-            
-            console.log((index + 1), foodNumber);
-            if (index + 1 != foodNumber){
-                gameOver = true;
-                foodNumber = 1;
-                return;
-            }
-            foodNumber ++;
-            
-            console.log((index + 1), foodNumber);
-            //alert(index + 1);
-            if (foodNumber == 6){
-                console.log(index + 1);
-                score = score + 3; // 2 extra points for completing a sequence
-            }
-            else{
-                score++; //increment score by 1 for eating a block
-            }
-            if (score > highScore){
-                highScore = score;
-            }
-            //snakeBody.push([[element[0], element[1]]]); // Pushing food position to snake body array
-            food.splice(index,1);
-            if (food.length == 0){
-                updateFoodPosition();            
-            }
-            console.log("hello")
-        }
-
-    /*if(snakeX === foodX && snakeY === foodY) {
-        foodSound.play();
-        updateFoodPosition();
-        snakeBody.push([foodX,foodY]); // Pushing food position to snake body array
-        score++; // increment score by 1
-        if (score > highScore){
-            highScore = score;
-
-        }*
-    
-        localStorage.setItem("high-score", highScore);
-        scoreElement.innerText = `Score: ${score}`;
-        highScoreElement.innerText = `High Score: ${highScore}`;
-    });*/
     if (gameOver){
         return;
     }
@@ -309,7 +177,6 @@ const initGame = () => {
         }
             
     }
-    // Setting first element of snake body to current snake position
 
     // Checking if the snake's head is out of wall, if so setting gameOver to true
     if(snakeX <= 0 || snakeX > gridsize || snakeY <= 0 || snakeY > gridsize) {
@@ -321,7 +188,7 @@ const initGame = () => {
             snakeX = 1;
         }
     }
-    snakeBody[0] = [snakeX, snakeY]; 
+    snakeBody[0] = [snakeX, snakeY]; // Setting first element of snake body to current snake position
     for (let i = 0; i < snakeBody.length; i++) {
         // Adding a div for each part of the snake's body
         html += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
@@ -330,8 +197,7 @@ const initGame = () => {
             gameOver = true;
         }
     }
-    let html2 = "";
-    //console.log(leaderList,typeof(leaderList));
+    let html2 = ""; //add leaderboard names to the leaderboard display
     leaderList.forEach((element, index) => {
         html2 += `<div class = "leader grid-row-start = ${index + 1} grid-column-start = 1">${element[1]} ${element[0]}</div>`
     });
@@ -340,10 +206,5 @@ const initGame = () => {
 }
 
 updateFoodPosition();
-/*while (true) {
-    duration = Date() - startTime;
-    setTimeout(initGame, 1000/duration);
-    document.addEventListener("keydown", changeDirection);
-  }*/
-setIntervalId = setInterval(initGame, 100);
+setIntervalId = setInterval(gameEngine, 100); //starts game loop
 document.addEventListener("keydown", changeDirection);
